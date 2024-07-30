ee0aed2516febf9c2c2de14fb1420e31e896029c
Arnaud DE SAINT JEAN <arnaud.dsj@gmail.com>
2024-07-30 11:01:46 +0200

    up port

--- server/server.py
+++ server/server.py
@@ -244,4 +244,4 @@ async def crawl(request: CrawlRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5202)
